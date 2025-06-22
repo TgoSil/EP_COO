43da8a4cf56a79c2ea46d47f12f83a40fa44030c
soneca3196 <167367109+soneca3196@users.noreply.github.com>
2025-06-21 22:49:10 -0300

    Add files via upload
    classses powerups

---
+++ src/classes/PowerUp.java
@@ -0,0 +1,29 @@
+package classes;
+
+
+/*Classe Power Ups que implementa a interface Entidade e será usada no PowerUps de Invulnerabilidade e TiroRapido */
+public abstract class PowerUp implements Entidade {
+    protected Ponto2D ponto;
+    protected double raio = 10.0;
+
+    public PowerUp(double x, double y) {
+        this.ponto = new Ponto2D(x, y, 0, 0.1);
+    }
+
+    @Override
+    public boolean atualizaEstado(long deltaTime, long currentTime, double limitador) {
+        ponto.setY(ponto.getY() + ponto.getvY() * deltaTime);
+        return ponto.getY() < limitador;
+    }
+
+    public boolean colisao(Player player) {
+        double dx = ponto.getX() - player.getX();
+        double dy = ponto.getY() - player.getY();
+        double dist = Math.sqrt(dx * dx + dy * dy);
+        return dist < (raio + player.getRaio());
+    }
+
+    public abstract void aplicarEfeito(Player player, long currentTime);
+
+    public abstract void desenha(long currentTime);
+}
